fbce6f64d91b867a8ee6f78169623e4b02c5e297
qodot <waitingforqodot@gmail.com>
2026-05-17 22:34:56 +0900

    test: FakeLLM ETS 테이블 소유자 안정화

--- pado/test/support/fake_llm.ex
+++ pado/test/support/fake_llm.ex
@@ -3,6 +3,7 @@ defmodule Pado.Test.FakeLLM do
   # worker process(다른 PID)가 caller chain을 통해 owner를 찾아 응답을 조회한다.
 
   @table :pado_test_fake_llm
+  @table_owner :pado_test_fake_llm_owner
 
   def setup_owner do
     init_table()
@@ -50,12 +51,63 @@ defmodule Pado.Test.FakeLLM do
   end
 
   defp init_table do
-    if :ets.whereis(@table) == :undefined do
-      :ets.new(@table, [:set, :public, :named_table])
+    case :ets.whereis(@table) do
+      :undefined -> ensure_table_owner()
+      _ -> :ok
+    end
+  end
+
+  defp ensure_table_owner do
+    case Process.whereis(@table_owner) do
+      nil -> start_table_owner()
+      pid -> ensure_table(pid)
+    end
+  end
+
+  defp start_table_owner do
+    pid = spawn(fn -> table_owner_loop() end)
+
+    try do
+      Process.register(pid, @table_owner)
+      ensure_table(pid)
+    rescue
+      ArgumentError ->
+        send(pid, :stop)
+
+        case Process.whereis(@table_owner) do
+          nil -> ensure_table_owner()
+          owner -> ensure_table(owner)
+        end
+    end
+  end
+
+  defp ensure_table(pid) do
+    send(pid, {:ensure_table, self()})
+
+    receive do
+      {@table_owner, :ready} -> :ok
+    after
+      1_000 -> exit(:fake_llm_table_owner_timeout)
+    end
+  end
+
+  defp table_owner_loop do
+    receive do
+      {:ensure_table, caller} ->
+        if :ets.whereis(@table) == :undefined do
+          :ets.new(@table, [:set, :public, :named_table])
+        end
+
+        send(caller, {@table_owner, :ready})
+        table_owner_loop()
+
+      :stop ->
+        :ok
     end
   end
 
   defp find_owner do
+    init_table()
     callers = [self() | Process.get(:"$callers", [])]
 
     Enum.find(callers, fn pid ->
