da907e0c58cc1d8c29bad7f601098cf795844f8b
qodot <waitingforqodot@gmail.com>
2026-05-30 23:10:47 +0900

    Add markdown rendering dependencies

--- pado_web/mix.exs
+++ pado_web/mix.exs
@@ -38,6 +38,8 @@ defmodule PadoWeb.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.1.0"},
+      {:earmark, "~> 1.4"},
+      {:html_sanitize_ex, "~> 1.5"},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
